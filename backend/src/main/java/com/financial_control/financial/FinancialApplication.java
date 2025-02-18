339c9c8ea0eecb5976c3776f3881f7ddf20429ce
vinilazzarini <viniciuslazzarini@gmail.com>
2025-02-18 09:58:38 -0300

    feat: Initial setup

---
+++ backend/src/main/java/com/financial_control/financial/FinancialApplication.java
@@ -0,0 +1,13 @@
+package com.financial_control.financial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/financial_control/financial/FinancialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financial_control.financial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
